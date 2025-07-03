5c48a4ef1dcf463de485313cdf813b0f5bbb0b61
KaranBhaskar <karanbhaskar014@gmail.com>
2025-07-02 21:30:00 -0400

    fix: @theme color namespace

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Community from './pages/Community.jsx';
+import Layout from './pages/Layout.jsx';
+import ResumeHelper from './pages/ResumeHelper.jsx';
+import ArticleGenerator from './pages/ArticleGenerator.jsx';
+import UserDashboard from './pages/UserDashboard.jsx';
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the React Application!</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/community" element={<Community/>}/>
+        <Route path="/layout" element={<Layout/>}/> 
+        <Route path="/resume-helper" element={<ResumeHelper/>}/>
+        <Route path="/article-generator" element={<ArticleGenerator/>}/>
+        <Route path="/user-dashboard" element={<UserDashboard/>}/>  
+      </Routes>
     </div>
   );
 };
